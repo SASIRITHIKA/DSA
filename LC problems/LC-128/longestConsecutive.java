11557d9c690a9d394f5bf81fe1055eb698ee667a
sazz@2352 <sasirithikaselvaraj200@gmail.com>
2026-03-13 11:58:37 +0530

    first commit

---
+++ LC problems/LC-128/longestConsecutive.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0)return 0;
+        int c=1, max=1;
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1])continue;
+            else if(nums[i]==nums[i-1]+1)c++;
+            else {
+                max=Math.max(max,c); c=1;
+           }
+        }
+        return Math.max(max, c);
+    }
+}
